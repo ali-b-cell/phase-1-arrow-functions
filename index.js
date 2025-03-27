bc46ac6ba6c81a41d46c18bf9321a0b55297cc2f
alice <alice.nthenge@student.moringaschool.com>
2025-03-28 00:41:48 +0300

    Completed lab

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const divide = function(a, b) {
+  return a / b;
+};
+console.log(divide(10, 2)); 
+
+
+const multiply = (a, b) => a * b;
+console.log(multiply(3, 4));
+
+
+const greetUser = (name) => {
+  console.log(`Hello, ${name}!`);
+  return `Welcome, ${name}!`;
+};
+console.log(greetUser("Alice")); 
+
+
+const square = x => x * x;
+console.log(square(5)); 
+
+
+const add = (a, b) => a + b;
+console.log(add(3, 7)); 
